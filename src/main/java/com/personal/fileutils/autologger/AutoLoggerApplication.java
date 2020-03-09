0bdb5968832fbf0b6e9e815adbf81421910475b8
Alec <rcsmith.alec@gmail.com>
2020-03-09 20:28:16 +0000

    Created project

---
+++ src/main/java/com/personal/fileutils/autologger/AutoLoggerApplication.java
@@ -0,0 +1,15 @@
+package com.personal.fileutils.autologger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLoggerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AutoLoggerApplication.class, args);
+    }
+
+}
